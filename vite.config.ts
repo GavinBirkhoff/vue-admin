f660944876220da5257ef6e00da8f028a358eb22
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2024-06-07 17:35:29 +0800

    chore: 添加别名和自定义端口

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,22 @@ export default defineConfig({
   plugins: [vue(), VueSetupExtend()],
   resolve: {
     alias: {
+      '~': fileURLToPath(new URL('./', import.meta.url)),
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    // https://cn.vitejs.dev/config/shared-options.html#resolve-extensions
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 9527,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
